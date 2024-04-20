aa20bb70dcc72fb8f77031cfa88f07aaf563151c
MaheshMLOPS <maheshn@atomstate.com>
2024-04-20 08:34:02 +0530

    some config changes

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
+
+  
+  constructor(public http : HttpClient){}
+
+  getData()
+  {
+    this.http.get('localhost:8080/getTasks').subscribe((data:any)=>{
+      console.log(data);
+    });
+  }
+
+
 
 }
